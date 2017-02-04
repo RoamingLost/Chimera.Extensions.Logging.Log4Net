370bc898e1a706079de809aca5b5baa94f932dea
John Stanley <jstanley@roaminglost.com>
2017-02-04 15:00:12 -0800

    Log implementation

--- src/Chimera.Extensions.Logging.Log4Net/Log4NetLogger.cs
+++ src/Chimera.Extensions.Logging.Log4Net/Log4NetLogger.cs
@@ -20,11 +20,62 @@
 
         public bool IsEnabled(LogLevel logLevel)
         {
-            return false;
+            switch (logLevel)
+            {
+                case LogLevel.Trace:
+                case LogLevel.Debug:
+                    return _log.IsDebugEnabled;
+                case LogLevel.Information:
+                    return _log.IsInfoEnabled;
+                case LogLevel.Warning:
+                    return _log.IsWarnEnabled;
+                case LogLevel.Error:
+                    return _log.IsErrorEnabled;
+                case LogLevel.Critical:
+                    return _log.IsFatalEnabled;
+                case LogLevel.None:
+                    return false;
+                default:
+                    throw new ArgumentException($"Unknown log level {logLevel}.", nameof(logLevel));
+            }
         }
 
         public void Log<TState>(LogLevel logLevel, EventId eventId, TState state, Exception exception, Func<TState, Exception, string> formatter)
         {
+            if (!IsEnabled(logLevel)) return;
+            if (formatter == null)
+            {
+                throw new ArgumentNullException(nameof(formatter));
+            }
+
+            var message = formatter(state, exception);
+            if (string.IsNullOrEmpty(message)) return;
+
+            switch (logLevel)
+            {
+                case LogLevel.Trace:
+                case LogLevel.Debug:
+                    _log.Debug(message, exception);
+                    break;
+                case LogLevel.Information:
+                    _log.Info(message, exception);
+                    break;
+                case LogLevel.Warning:
+                    _log.Warn(message, exception);
+                    break;
+                case LogLevel.Error:
+                    _log.Error(message, exception);
+                    break;
+                case LogLevel.Critical:
+                    _log.Fatal(message, exception);
+                    break;
+                case LogLevel.None:
+                    break;
+                default:
+                    _log.Warn($"Encountered unknown log level {logLevel}, writing out as Info.");
+                    _log.Info(message, exception);
+                    break;
+            }
         }
     }
 }
