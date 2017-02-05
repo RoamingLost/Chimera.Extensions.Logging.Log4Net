2c0efb3eaddccd9a3da863b4f341124b7daf657a
John Stanley <jstanley@roaminglost.com>
2017-02-05 09:03:15 -0800

    Initial test project check in

---
+++ test/Chimera.Extensions.Logging.Log4Net.Tests/Log4NetSettingsTests.cs
@@ -0,0 +1,14 @@
+﻿namespace Chimera.Extensions.Logging.Log4Net.Tests
+{
+    using System;
+    using Xunit;
+
+    public class Log4NetSettingsTests
+    {
+        [Fact]
+        public void Test1() 
+        {
+            Assert.True(true);
+        }
+    }
+}
