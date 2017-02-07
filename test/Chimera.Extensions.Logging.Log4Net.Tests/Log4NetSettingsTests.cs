acca20e32b210e9d59e6420acb83b43c5b7407bc
John Stanley <jstanley@roaminglost.com>
2017-02-07 09:01:33 -0800

    Settings unit tests

--- test/Chimera.Extensions.Logging.Log4Net.Tests/Log4NetSettingsTests.cs
+++ test/Chimera.Extensions.Logging.Log4Net.Tests/Log4NetSettingsTests.cs
@@ -1,10 +1,12 @@
 ﻿namespace Chimera.Extensions.Logging.Log4Net.Tests
 {
     using System;
+    using System.Collections.Generic;
     using Chimera.Extensions.Logging.Log4Net;
     using log4net;
     using log4net.Core;
     using log4net.Repository;
+    using Microsoft.Extensions.Configuration;
     using Microsoft.Extensions.Logging;
     using Moq;
     using Xunit;
@@ -47,6 +49,44 @@
 
             // Assert
             logMock.Verify(m => m.Debug(It.IsAny<object>(), It.IsAny<Exception>()), Times.AtLeastOnce);
+        }
+
+        [Fact]
+        public void SettingsDefault()
+        {
+            // Arrange
+
+
+            // Act
+            var settings = new Log4NetSettings();
+
+            // Assert
+            Assert.Equal(1, settings.GlobalContextProperties.Count);
+            Assert.Equal("log4net.config", settings.ConfigFilePath);
+            Assert.False(settings.Watch);
+        }
+
+        [Fact]
+        public void SettingsFromConfiguration()
+        {
+            // Arrange
+            var dict = new Dictionary<string, string>
+            {
+                { "ConfigFilePath", "test.config" },
+                { "Watch", "true" }
+            };
+            var config = new ConfigurationBuilder()
+                .AddInMemoryCollection(dict)
+                .Build();
+
+            // Act
+            var settings = config.Get<Log4NetSettings>();
+
+            // Assert
+            Assert.NotNull(settings);
+            Assert.Equal(1, settings.GlobalContextProperties.Count);
+            Assert.Equal("test.config", settings.ConfigFilePath);
+            Assert.True(settings.Watch);
         }
     }
 }
